4eb480fc4c8c5b9753b1a4584dad764786639cac
Matt Geimer <mgeimer@gmail.com>
2016-02-15 11:58:43 -0800

    Added NuGet for ADAL

--- graphconsole/Program.cs
+++ graphconsole/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;  //for the http requests
 using System.Net;
-
+using Microsoft.IdentityModel.Clients.ActiveDirectory; //for ADAL auth.
 
 namespace graphconsole
 {
@@ -14,8 +13,7 @@ namespace graphconsole
         static void Main(string[] args)
         {
             string URL = args[0];
-            WebRequest request;
-            request = WebRequest.Create(URL);
+            WebRequest request = WebRequest.Create(URL);
             Stream response = request.GetResponse().GetResponseStream();
             StreamReader readresponse = new StreamReader(response);
             string line = "";
