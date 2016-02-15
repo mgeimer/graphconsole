d82d2b7787ed2a3fef8c3e41147a576f17674bdd
Matt Geimer <mgeimer@gmail.com>
2016-02-15 11:10:56 -0800

    Restored old project code. GET request of URL on command line and spew it out in console.

--- graphconsole/Program.cs
+++ graphconsole/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;  //for the http requests
+using System.Net;
+
 
 namespace graphconsole
 {
@@ -10,7 +13,19 @@ namespace graphconsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("KORA! Noooo!");
+            string URL = args[0];
+            WebRequest request;
+            request = WebRequest.Create(URL);
+            Stream response = request.GetResponse().GetResponseStream();
+            StreamReader readresponse = new StreamReader(response);
+            string line = "";
+            int i = 0;
+            while (line != null)
+            {
+                i++;
+                line = readresponse.ReadLine();
+                if (line != null) Console.WriteLine(line);
+            }
         }
     }
 }
